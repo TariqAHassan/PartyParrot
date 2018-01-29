5716ae62f1b2d888c193aeba7c6ed9fb0ac8f246
TariqAHassan <tariqaahassan@gmail.com>
2018-01-29 11:41:49 -0800

    Bug Fix

--- party_parrot/__init__.py
+++ party_parrot/__init__.py
@@ -1,9 +1,9 @@
 from party_parrot.parrot_engine import ParrotLang
 
 
-def to_parrot(string, key=1):
-    return ParrotLang(encryption_key=key).to_parrot(string)
+def to_parrot(string, key=1, copy=False):
+    return ParrotLang(encryption_key=key).to_parrot(string, copy=copy)
 
 
-def from_parrot(string, key=1):
-    return ParrotLang(encryption_key=key).from_parrot(string)
+def from_parrot(string, key=1, copy=False):
+    return ParrotLang(encryption_key=key).from_parrot(string, copy=copy)
