40dd1cd1c0246ab938fa64b97cf29baefe77f2e8
TariqAHassan <tariqaahassan@gmail.com>
2018-01-29 10:31:38 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except:
+        return 'Please see: https://github.com/TariqAHassan/PartyParrot.'
+
+
+setup(
+    name='party_parrot',
+    version='0.1',
+    author='Tariq A. Hassan',
+    author_email='laterallattice@gmail.com',
+    description=('The future is parrot.'),
+    long_description=read('docs/README.md'),
+    license='BSD',
+    keywords='Party Parrot',
+    url='https://github.com/TariqAHassan/PartyParrotLang.git',
+    download_url='https://github.com/TariqAHassan/PartyParrot/archive/v0.1.tar.gz',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Natural Language :: English',
+                 'Intended Audience :: Science/Research',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'License :: OSI Approved :: BSD License'
+    ],
+    include_package_data=True
+)
