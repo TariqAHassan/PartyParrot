ec920dfe3640db52b238c08a8b0c1ee5b25acfde
TariqAHassan <tariqaahassan@gmail.com>
2018-01-29 11:25:11 -0800

    Bug Fix for key

--- party_parrot/parrot_engine.py
+++ party_parrot/parrot_engine.py
@@ -4,8 +4,7 @@
     ~~~~~~
 
 """
-import random
-from party_parrot._slack_parrots import parrots
+from sklearn.utils import shuffle
 from string import ascii_lowercase as letters
 from party_parrot._utils import parrot_splitter
 
@@ -13,8 +12,10 @@ from party_parrot._utils import parrot_splitter
 class ParrotLang(object):
     def __init__(self, encryption_key=1):
         # Shuffle
-        random.seed(encryption_key)
-        random.shuffle(parrots)
+        from party_parrot._slack_parrots import parrots
+        parrots = shuffle(parrots, random_state=encryption_key)
+
+        # Look up dicts
         self._letter_par = {letter: par for letter, par in zip(letters, parrots)}
         self._par_letter = {v: k for k, v in self._letter_par.items()}
 
