c376d973b9bca0574d633f58019c33444ecbfe66
TariqAHassan <tariqaahassan@gmail.com>
2018-01-31 01:33:23 -0800

    Update parrots

--- party_parrot/_slack_parrots.py
+++ party_parrot/_slack_parrots.py
@@ -8,10 +8,10 @@ from party_parrot._utils import get_parrots
 
 slack_parrots = """
 :aussie_parrot: :blonde_sassy_parrot: :bored_parrot: :chill_parrot: :christmas_parrot: :coffee_parrot:
-:confused_parrot: :conga_parrot: :dark_beer_parrot: :deal_with_it_parrot: :explody_parrot: :fieri_parrot:
+:confused_parrot: :conga_parrot: :dark_beer_parrot: :deal_with_it_parrot: :angel_parrot: :fieri_parrot:
 :goth_parrot: :margarita_parrot: :parrot_dad: :party_parrot: :popcorn_parrot: :sad_parrot: :ship_it_parrot:
 :ski_parrot: :slow_parrot: :triplets_parrot: :twins_parrot: :witness_protection_parrot: :evil_parrot:
-:sleeping_parrot: :papal_parrot:
+:sleeping_parrot:
 """
 
 parrots = get_parrots(slack_parrots)
