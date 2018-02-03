f2db8fd33df755ca643ee7da7717494301b8c264
TariqAHassan <tariqaahassan@gmail.com>
2018-02-02 17:36:05 -0800

    Update default param. values; add docstrings.

--- party_parrot/__init__.py
+++ party_parrot/__init__.py
@@ -6,9 +6,35 @@ from party_parrot.parrot_engine import ParrotLang
 _pl = ParrotLang()
 
 
-def to_parrot(string, key=1, copy=False):
+def to_parrot(string, key=None, copy=False):
+    """
+
+    Convert to parrot.
+
+    Args:
+        string (str): any string
+        key (int): a key to encrypt parrot messages. If None, 1 will be used.
+        copy (bool): if True, copy to clipboard.
+
+    Returns
+        str: the converted message.
+
+    """
     return _pl.to_parrot(string, key=key, copy=copy)
 
 
-def from_parrot(string, key=1, copy=False):
+def from_parrot(string, key=None, copy=False):
+    """
+
+    Convert from parrot.
+
+    Args:
+        string (str): any string
+        key (int): a key to encrypt parrot messages. If None, 1 will be used.
+        copy (bool): if True, copy to clipboard.
+
+    Returns
+        str: the converted message.
+
+    """
     return _pl.from_parrot(string, key=key, copy=copy)
